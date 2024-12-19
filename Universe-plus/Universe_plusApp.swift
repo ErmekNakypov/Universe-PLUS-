75d3c601cce72b4bc8ce15bb331cabd748de2e14
spacermek <spacermek@gmail.com>
2024-12-19 21:47:01 +0600

    Initial Commit

---
+++ Universe-plus/Universe_plusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Universe_plusApp.swift
+//  Universe-plus
+//
+//  Created by Накыпов Эрмек on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Universe_plusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
